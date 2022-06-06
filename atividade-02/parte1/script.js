5df55e69a033c1f3c7cb33d4325054988e18ef14
marlissonls <marlisson.business@gmail.com>
2022-06-06 09:24:23 -0300

    convertendo as variáveis first e secondNumber para número com a função parseInt()

--- atividade-02/parte1/script.js
+++ atividade-02/parte1/script.js
@@ -1,6 +1,6 @@
 function compareNumbers() {
-    var firstNumber = document.getElementById("first-number").value;
-    var secondNumber = document.getElementById("second-number").value;
+    var firstNumber = parseInt(document.getElementById("first-number").value);
+    var secondNumber = parseInt(document.getElementById("second-number").value);
 
     if (firstNumber > secondNumber)
         document.getElementById("result").innerHTML = "O primeiro número é MAIOR que o segundo número";
