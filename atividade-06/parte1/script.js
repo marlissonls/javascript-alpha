284104f469e650c2f60a1b03f1ffbe9703968490
marlissonls <marlisson.business@gmail.com>
2022-06-14 20:01:29 -0300

    nova funcionalidade imagem inicial

--- atividade-06/parte1/script.js
+++ atividade-06/parte1/script.js
@@ -1,25 +1,35 @@
-const contagem = setTimeout(detonarBomba, 3000);
 const bomba = document.getElementById('bomb-img');
+const armarB = document.querySelector('#button');
+const desarmarB = document.getElementById('bomb-img');
 const bombPlantedAudio = document.getElementById("planted-audio");
 const bombDefusedAudio = document.getElementById("defused-audio");
 const bombExplosionAudio = document.getElementById("explosion-audio");
+let contagem;
 
-bombPlantedAudio.volume = 0.9;
-bombPlantedAudio.play();
+//armarB.addEventListener("click", armarBomba);
+desarmarB.addEventListener("click", desarmarBomba);
 
-bomba.addEventListener("click", desarmarBomba);
-
-function desarmarBomba () {
-    clearTimeout (contagem);
-    bomba.src = "images/img2.png";
-    bombDefusedAudio.volume = 0.9;
-    bombDefusedAudio.play();
-    document.querySelector("h2").innerHTML = "THE BOM HAS BEEN DEFUSED";
+function armarBomba () {
+    bomba.src = "images/img1.png";
+    bombPlantedAudio.volume = 1.0;
+    bombPlantedAudio.play();
+    document.querySelector("h2").innerHTML = "THE BOM HAS BEEN PLANTED";
+    contagem = setTimeout(detonarBomba, 3000);
 }
 
 function detonarBomba () {
     bomba.src = "images/explosao.png";
-    bombExplosionAudio.volume = 0.9;
+    bombExplosionAudio.volume = 1.0;
     bombExplosionAudio.play();
-    bomba.removeEventListener("click", desarmarBomba);
+    desarmarB.removeEventListener("click", desarmarBomba);
+    document.querySelector("h2").innerHTML = "OH NO!!<br><button type='button' onclick='armarBomba ()'>TRY AGAIN (CLICK HERE)</button>";
 }
+
+function desarmarBomba () {
+    clearTimeout (contagem);
+    bomba.src = "images/img2.png";
+    bombDefusedAudio.volume = 1.0;
+    bombDefusedAudio.play();
+    document.querySelector("h2").innerHTML = "THE BOM HAS BEEN DEFUSED<br><button type='button' onclick='armarBomba ()'>TRY AGAIN (CLICK HERE)</button>";
+}
+
