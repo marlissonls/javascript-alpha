2098bc156e56bb6dffb772b1e3147f5774342a83
marlissonls <marlisson.business@gmail.com>
2022-06-15 01:19:21 -0300

    atividade-06 questão 2 - erro no timer após a primeira explosão

--- atividade-06/parte2/script.js
+++ atividade-06/parte2/script.js
@@ -1,18 +1,55 @@
+/*
+let remainingTime = 10;
+let myInterval = setInterval(myTimer,1000);
+
+function myTimer () {
+    remainingTime--;
+    let timing = document.getElementById('timer');
+    timing.innerHTML = remainingTime;
+    if (remainingTime <= 3) {
+        timing.style.color = "red";
+    }
+    if (remainingTime === 0) {
+        clearInterval (myInterval);;
+    }
+}
+*/
+
 const bomba = document.getElementById('bomb-img');
 const armarB = document.querySelector('#button');
 const desarmarB = document.getElementById('bomb-img');
 const bombPlantedAudio = document.getElementById("planted-audio");
 const bombDefusedAudio = document.getElementById("defused-audio");
 const bombExplosionAudio = document.getElementById("explosion-audio");
-let contagem;
+
+
+let remainingTime;
+let decreaseTime;
+let displayTimer;
 
 function armarBomba () {
+    remainingTime = 5+1;
     bomba.src = "images/img1.png";
     bombPlantedAudio.volume = 1.0;
     bombPlantedAudio.play();
-    document.querySelector("h2").innerHTML = "THE BOMB HAS BEEN PLANTED<br><button type='button'>WAIT 10 SECONDS OR CLICK ON BOMB</button>";
+    document.querySelector("h2").innerHTML = "THE BOMB HAS BEEN PLANTED<br><button type='button'>WAIT OR CLICK ON BOMB</button>";
+    decreaseTime = setInterval (timer, 1000);
     desarmarB.addEventListener("click", desarmarBomba);
-    contagem = setTimeout(detonarBomba, 10000);
+}
+
+function timer () {
+    remainingTime--;
+    displayTimer = document.querySelector("#timer");
+    if (remainingTime >= 10) {
+        displayTimer.innerHTML = "00:" + remainingTime;
+
+    } else if (remainingTime < 10 && remainingTime > 0) {
+        displayTimer.innerHTML = "00:0" + remainingTime;
+        
+    } else if (remainingTime === 0) {
+        displayTimer.innerHTML = "00:0" + remainingTime;
+        detonarBomba ();
+    }
 }
 
 function detonarBomba () {
@@ -21,10 +58,11 @@ function detonarBomba () {
     bombExplosionAudio.play();
     desarmarB.removeEventListener("click", desarmarBomba);
     document.querySelector("h2").innerHTML = "OH NO!!<br><button type='button' onclick='retry ()'>TRY AGAIN</button>";
+    
 }
 
 function desarmarBomba () {
-    clearTimeout (contagem);
+    clearInterval (decreaseTime);
     bomba.src = "images/img2.png";
     bombDefusedAudio.volume = 1.0;
     bombDefusedAudio.play();
@@ -35,4 +73,5 @@ function desarmarBomba () {
 function retry () {
     bomba.src = "images/img0.png";
     document.querySelector("h2").innerHTML = "NOTHING HAPPENING...<br><button type='button' onclick='armarBomba ()'>START</button>";
+    document.querySelector("#timer").innerHTML = "TIMER";
 }
