706bbe700a02f105ba42004e6b89f0332692bac9
marlissonls <marlisson.business@gmail.com>
2022-06-16 15:29:44 -0300

    atividade-06 parte3 finalizado

--- atividade-06/parte3/script.js
+++ atividade-06/parte3/script.js
@@ -1,37 +1,49 @@
 let minutos;
 let segundos;
 let decreaseTime;
+let totalSecs;
 const alarme = document.querySelector("#alarm");
+const timer = document.querySelector("#timer");
+const button = document.querySelector("#button");
 
 function start () {
     minutos = parseInt(document.querySelector("#minutos").value);
     segundos = parseInt(document.querySelector("#segundos").value);
     decreaseTime = setInterval (decreaseSecs, 1000);
-    document.querySelector("#button").innerHTML = '<button type="button" id="stop-timer" onclick="stop()">PARAR</button>';
+    button.innerHTML = '<button type="button" id="stop-timer" onclick="stop()">PARAR</button>';
+    totalSecs = segundos + 60*minutos;
 }
 
 function stop () {
     clearInterval(decreaseTime);
     alarme.load();
-    document.querySelector("#button").innerHTML = '<button type="button" id="start-timer" onclick="start()">INICIAR</button>';
+    button.innerHTML = '<button type="button" id="start-timer" onclick="start()">INICIAR</button>';
 }
 
 function decreaseSecs () {
     //Os primeiros 4 if()s servem para ajustar o display do timer.
+    if((segundos + 60*minutos) <= 0.05*totalSecs) {
+        timer.style.color = "#ff3737";
+        timer.style.textShadow = "0 0 15px #cc0000";
+    }else {
+        timer.style.color = "#37ff37";
+        timer.style.textShadow = "0 0 15px #00cc00";
+    }
+
     if (minutos >= 10 && segundos >= 10) {
-        document.querySelector("#timer").innerHTML = minutos + ":" + segundos;
+        timer.innerHTML = minutos + ":" + segundos;
     };
 
     if (minutos >= 10 && segundos < 10) {
-        document.querySelector("#timer").innerHTML = minutos + ":0" + segundos;
+        timer.innerHTML = minutos + ":0" + segundos;
     };
 
     if (minutos < 10 && segundos >= 10) {
-        document.querySelector("#timer").innerHTML = "0" + minutos + ":" + segundos;
+        timer.innerHTML = "0" + minutos + ":" + segundos;
     };
     
     if (minutos < 10 && segundos < 10) {
-        document.querySelector("#timer").innerHTML = "0" + minutos + ":0" + segundos;
+        timer.innerHTML = "0" + minutos + ":0" + segundos;
     };
 
     //Estes outros 4 if()s servem para decrementar os minutos e segundos ou reatribuir os segundos com o valor 59
